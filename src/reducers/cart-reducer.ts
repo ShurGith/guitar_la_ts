98d6ee53ff78aca684930a6bb45a4d44160062bf
ShurGith <75266693+ShurGith@users.noreply.github.com>
2025-07-17 14:35:28 +0200

    fin

--- src/reducers/cart-reducer.ts
+++ src/reducers/cart-reducer.ts
@@ -1,19 +1,110 @@
-import { db } from '../data/db';
-import type { Guitar, CartItem } from '../types';
+import { db } from "../data/db";
+import { CartItem, Guitar } from "../types";
 
 export type CartActions =
-    { type: 'ADD_TO_CART', payload: { guitar: Guitar } } |
-    { type: 'REMOVE_FROM_CART', payload: { id: Guitar['id'] } } |
-    { type: 'INCREASE_QUANTITY', payload: { id: Guitar['id'] } } |
-    { type: 'DECREASE_QUANTITY', payload: { id: Guitar['id'] } } |
-    { type: 'CLEAR_CART' };
+  { type: 'add-to-cart', payload: { item: Guitar } } |
+  { type: 'remove-from-cart', payload: { id: Guitar['id'] } } |
+  { type: 'decrease-quantity', payload: { id: Guitar['id'] } } |
+  { type: 'increase-quantity', payload: { id: Guitar['id'] } } |
+  { type: 'clear-cart' }
 
 export type CartState = {
-    data: Guitar[];
-    cart: CartItem[];
+  data: Guitar[]
+  cart: CartItem[]
+}
+
+const initialCart = (): CartItem[] => {
+  const localStorageCart = localStorage.getItem('cart')
+  return localStorageCart ? JSON.parse(localStorageCart) : []
 }
 
 export const initialState: CartState = {
-    data: db,
-    cart: []
+  data: db,
+  cart: initialCart()
+}
+
+const MIN_ITEMS = 1
+const MAX_ITEMS = 5
+
+export const cartReducer = (
+  state: CartState = initialState,
+  action: CartActions
+) => {
+
+  if (action.type === "add-to-cart") {
+    const itemExists = state.cart.find(guitar => guitar.id === action.payload.item.id)
+    let updatedCart: CartItem[] = []
+    if (itemExists) { // item exists in the cart
+      updatedCart = state.cart.map(item => {
+        if (item.id === action.payload.item.id) {
+          if (item.quantity < MAX_ITEMS) {
+            return { ...item, quantity: item.quantity + 1 }
+          } else {
+            return item
+          }
+        } else {
+          return item
+        }
+      })
+    } else {
+      const newItem: CartItem = { ...action.payload.item, quantity: 1 }
+      updatedCart = [...state.cart, newItem]
+    }
+
+    return {
+      ...state,
+      cart: updatedCart
+    }
+  }
+
+  if (action.type === 'remove-from-cart') {
+    const cart = state.cart.filter(item => item.id !== action.payload.id)
+    return {
+      ...state,
+      cart
+    }
+  }
+
+  if (action.type === 'decrease-quantity') {
+    const cart = state.cart.map(item => {
+      if (item.id === action.payload.id && item.quantity > MIN_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity - 1
+        }
+      }
+      return item
+    })
+
+    return {
+      ...state,
+      cart
+    }
+  }
+
+  if (action.type === 'increase-quantity') {
+    const cart = state.cart.map(item => {
+      if (item.id === action.payload.id && item.quantity < MAX_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity + 1
+        }
+      }
+      return item
+    })
+
+    return {
+      ...state,
+      cart
+    }
+  }
+
+  if (action.type === 'clear-cart') {
+    return {
+      ...state,
+      cart: []
+    }
+  }
+
+  return state
 }
